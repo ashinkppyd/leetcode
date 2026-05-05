c07510569a5a85eb9edf2dc0879ab53ddccbd500
Ashin kp <ashinkppyd@gmail.com>
2026-05-05 10:50:27 +0530

    Time: 0 ms (100%), Space: 12.5 MB (21.04%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def xorOperation(self, n, start):
+        result = 0
+        
+        for i in range(n):
+            num = start + 2 * i
+            result ^= num
+        
+        return result
+        
